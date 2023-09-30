c146406b54a7edb1e8e82bf3234c0eca20cd0dd3
PerryEx64 <brianisrael74@gmail.com>
2023-09-30 12:26:59 -0600

    creacion carpeta build

---
+++ public/build/assets/Territorios-eed49cf2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as r,f as o}from"./app-5c141909.js";const t={};function c(s,n){return r(),o("h1",null,"aqui muestra eso")}const f=e(t,[["render",c]]);export{f as default};
